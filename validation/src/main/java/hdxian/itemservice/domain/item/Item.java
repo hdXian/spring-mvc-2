e9e15ff271b33566e16d6061bd0c1bb20f474521
hdXian <jason123325@gmail.com>
2024-08-29 21:31:49 +0900

    #9 9-2 - apply Bean Validation annotation on class Item

--- validation/src/main/java/hdxian/itemservice/domain/item/Item.java
+++ validation/src/main/java/hdxian/itemservice/domain/item/Item.java
@@ -1,14 +1,26 @@
 package hdxian.itemservice.domain.item;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 
 import java.util.List;
 
 @Data // contains @Getter, @Setter, @RequiredArgsConstructor, @EqualsAndHashCode, @ToString
 public class Item {
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
