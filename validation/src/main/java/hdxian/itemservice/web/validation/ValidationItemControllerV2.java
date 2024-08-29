2b239f85d973ee9335e7d82906181c5c4940c094
hdXian <jason123325@gmail.com>
2024-08-29 19:57:01 +0900

    #7 7-14 - validation with webDataBinder(Annot InitBinder), Validated(Annot)

--- validation/src/main/java/hdxian/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hdxian/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,6 +10,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -25,6 +27,12 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
     private final ItemValidator itemValidator;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        log.info("init binder = {}", dataBinder);
+        dataBinder.addValidators(itemValidator);
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -228,7 +236,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(@ModelAttribute("item") Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         log.info("bindingResult.getObjectName() = {}", bindingResult.getObjectName());
@@ -238,6 +246,26 @@ public class ValidationItemControllerV2 {
         if (itemValidator.supports(Item.class)){
             itemValidator.validate(item, bindingResult);
         }
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "/validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true); // put into query parameter
+
+        // /validation/v2/items/1?status=true
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(@Validated @ModelAttribute("item") Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // @Validated -  dataBinder will validate object Item
+
+        log.info("bindingResult.getObjectName() = {}", bindingResult.getObjectName());
+        log.info("bindingResult.getTarget() = {}", bindingResult.getTarget());
 
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
