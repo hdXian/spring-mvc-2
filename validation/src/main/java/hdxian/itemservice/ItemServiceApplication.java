fe02f42bc07480cb5ca936c683bfb0cca21bc344
hdXian <jason123325@gmail.com>
2024-08-29 20:01:03 +0900

    #7 7-14-2 - global validator config

--- validation/src/main/java/hdxian/itemservice/ItemServiceApplication.java
+++ validation/src/main/java/hdxian/itemservice/ItemServiceApplication.java
@@ -1,7 +1,10 @@
 package hdxian.itemservice;
 
+import hdxian.itemservice.web.validation.ItemValidator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.validation.Validator;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -9,5 +12,10 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
+
+//	@Override // global validator config (implements WebMvcConfigurer needed)
+//	public Validator getValidator() {
+//		return new ItemValidator();
+//	}
 
 }
