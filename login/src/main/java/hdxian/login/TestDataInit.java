a6a1cda9f10e50e6f8b088a65cf7a98f690ea265
hdXian <jason123325@gmail.com>
2024-09-05 15:37:52 +0900

    #11 add test account on TestDataInit

--- login/src/main/java/hdxian/login/TestDataInit.java
+++ login/src/main/java/hdxian/login/TestDataInit.java
@@ -2,6 +2,8 @@ package hdxian.login;
 
 import hdxian.login.domain.item.Item;
 import hdxian.login.domain.item.ItemRepository;
+import hdxian.login.domain.member.Member;
+import hdxian.login.domain.member.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,11 +13,19 @@ import org.springframework.stereotype.Component;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("testItem1", 10000, 11));
         itemRepository.save(new Item("testItem2", 20000, 22));
+
+        // add test account
+        Member tester = new Member();
+        tester.setLoginId("test");
+        tester.setPassword("test!");
+        tester.setName("tester");
+        memberRepository.save(tester);
     }
 
 }
